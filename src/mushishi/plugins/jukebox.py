6460500f0a0c47e3021a407c662ec5304f40363a
Grayson Miller <grayson.miller124@gmail.com>
2022-11-11 09:35:17 +0000

    Added jukebox commands and structure.

--- src/mushishi/plugins/jukebox.py
+++ src/mushishi/plugins/jukebox.py
@@ -21,17 +21,32 @@ class Jukebox(Cog):
         if not os.path.isfile(self.bot.ch_path):
             with open(self.ch_path, mode='w+'):
                 pass
+    
+    @commands.group(pass_context=True)
+    async def yt(self, ctx):
+        """ Youtube jukebox commands """
+        pass
+    
+    @yt.command()
+    async def playing(self, ctx):
+        """ Get the currently playing song. """
+        if ctx.voice_client.is_playing():
+            await ctx.send(f"Now playing: {self.currently_playing}")
+        else:
+            await ctx.send("Nothing is playing.")
 
-    @commands.command()
-    async def yt_queue(self, ctx):
+    @yt.command()
+    async def queue(self, ctx):
+        """ List the current queue. """
         pretty_queue = '\n'.join([f"{i+1}. {title}" for i, (title, _) in enumerate(self.queue)])
         await ctx.send(f"Current queue: {pretty_queue}")
 
     def play_next(self, ctx):
         if len(self.queue) > 0:
-            self.currently_playing = self.queue.pop(0)
-            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(self.currently_playing[1]))
+            now_playing = self.queue.pop(0)
+            self.currently_playing = now_playing[0]
 
+            source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(now_playing[1]))
             ctx.voice_client.play(source, after=lambda _: self.play_next(ctx))
 
     async def play_result(self, ctx, result):
@@ -42,20 +57,20 @@ class Jukebox(Cog):
 
         fn = result['entries'][0]['id'] + '.' + result['entries'][0]['ext']
         fp = os.path.join(self.bot.resource_path, 'music', fn)
-        self.queue.append(title, fp) 
+        self.queue.append((title, fp)) 
 
         if self.vc is None or self.vc.channel.id != voice_channel.id:
             self.vc = await voice_channel.connect()
 
-        self.logger.info(ctx.voice_client, self.vc, self.vc.is_playing(), ctx.voice_client.is_playing())
         if ctx.voice_client.is_playing():
             await ctx.send(f"Queued for play (#{len(self.queue)}): {title}")
         else:
             await ctx.send(f"Now playing: {title}")
             self.play_next(ctx)
 
-    @commands.command(pass_context=True)
-    async def yt(self, ctx, *search):
+    @yt.command(pass_context=True)
+    async def play(self, ctx, *search):
+        """ Plays the first result from a youtube search, or adds it to the queue. """
         search = (' ').join(search)
 
         ydl_opts = {
@@ -85,9 +100,11 @@ class Jukebox(Cog):
         future = self.bot.loop.run_in_executor(None, YoutubeDL(ydl_opts).extract_info, search)
         future.add_done_callback(lambda f: asyncio.ensure_future(self.play_result(ctx, f.result())))
 
+
 async def setup(bot):
     await bot.add_cog(Jukebox(bot))
 
+
 async def teardown(bot):
-    bot.vc.disconnect()
+    await bot.vc.disconnect()
     bot.vc = None
